2817881432a111543265e688b0ee476d086fe4fe
ananprara <137501378+ananprara@users.noreply.github.com>
2025-04-16 15:30:42 +0530

    Setup initial testing framework and basic item tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,53 @@
 # -*- coding: utf-8 -*-
+"""
+Tests for Gilded Rose inventory system
+"""
 import unittest
-
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    """Test suite for the Gilded Rose inventory system"""
+
+    def test_regular_item_before_sell_date(self):
+        """Regular items should degrade by 1 before sell date"""
+        items = [Item("Regular Item", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(9, items[0].quality)
+
+    def test_regular_item_on_sell_date(self):
+        """Regular items should degrade by 2 on sell date"""
+        items = [Item("Regular Item", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_regular_item_after_sell_date(self):
+        """Regular items should degrade by 2 after sell date"""
+        items = [Item("Regular Item", -1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-2, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_regular_item_zero_quality(self):
+        """Item quality should never go negative"""
+        items = [Item("Regular Item", 5, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
+
+    def test_item_names_dont_change(self):
+        """Item names should never change"""
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
